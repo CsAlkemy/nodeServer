45715551a16c6c916c80f43442fdb541e072e5a6
AlkemyHossain <alkemy48@gmail.com>
2019-10-11 22:08:11 +0600

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
-var http = require('http'); // 1 - Import Node.js core module
+var http = require('http'); 
+var server = http.createServer(function (req, res) {  
 
-var server = http.createServer(function (req, res) {   // 2 - creating server
-
-    //handle incomming requests here..
+    
 
 });
 
-server.listen(5000); //3 - listen for any incoming requests
+server.listen(5000);
 
 console.log('Node.js web server at port 5000 is running..')
